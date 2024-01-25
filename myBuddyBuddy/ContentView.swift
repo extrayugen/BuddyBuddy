c4c3854c1420a3c120081a73c0ef19d393cf82bb
Jooyeong(Yugen) <supershicker@gmail.com>
2024-01-25 16:18:34 +0900

    Initial Commit

---
+++ myBuddyBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myBuddyBuddy
+//
+//  Created by t2023-m0031 on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myBuddyBuddy/myBuddyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBuddyBuddyApp.swift
+//  myBuddyBuddy
+//
+//  Created by t2023-m0031 on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct myBuddyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
